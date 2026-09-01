01ea1ed2d9b162b663ed08c14c8b0090d61f5a67
payal <payalbalotra441@gmail.com>
2026-09-01 10:39:25 +0530

    check

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Express with TypeScript! hi');
+  res.send('Hello from Express with TypeScript');
 });
 
 
